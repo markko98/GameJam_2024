b45b054c0206bff3373e8e27fed420fafed1a48a
markko98 <pavliskomarko@gmail.com>
2024-01-23 11:28:33 +0100

    gameplay scene and audio transition, cleanup

--- GameJam_2024/Assets/_Game/Scripts/Constants/Scenes.cs
+++ GameJam_2024/Assets/_Game/Scripts/Constants/Scenes.cs
@@ -1,10 +1,12 @@
 ﻿public struct SceneNames {
     public static string EntryPoint = "EntryPoint";
     public static string MainMenu = "MainMenu";
+    public static string Gameplay = "Gameplay";
 }
 
 public struct SceneOutlets
 {
     public static string EntryPoint = "EntryPointOutlet";
     public static string MainMenu = "MainMenuOutlet";
+    public static string Gameplay = "GameplayOutlet";
 }
