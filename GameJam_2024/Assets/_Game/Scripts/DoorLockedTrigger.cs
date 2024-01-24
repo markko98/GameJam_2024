0d41c241f2ff4e4558f11b1a3c9e31cb4afbc226
Mehmet Demirci <mdemirci@racunarstvo.hr>
2024-01-24 15:29:12 +0100

    Locked trigger notification

--- GameJam_2024/Assets/_Game/Scripts/DoorLockedTrigger.cs
+++ GameJam_2024/Assets/_Game/Scripts/DoorLockedTrigger.cs
@@ -1,16 +1,29 @@
 using DG.Tweening.Core.Easing;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DoorLockedTrigger : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             // TODO
             Debug.Log("Door Locked Sound");
+
+            text.text = "Ohh shit!! It's locked.";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.text = "";
         }
     }
 }
